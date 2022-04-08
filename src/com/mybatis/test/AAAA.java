bdd2d8d92c3341d8fb899099e84566f5f7da53c3
王文书 <1449737934@qq.com>
2022-04-08 09:50:20 +0800

    项目初始化master修改了AAAAA

--- src/com/mybatis/test/AAAA.java
+++ src/com/mybatis/test/AAAA.java
@@ -3,5 +3,6 @@ package com.mybatis.test;
 public class AAAA {
     public static void main(String[] args) {
         System.out.println("dsjcvghlsd");
+        System.out.println("master");
     }
 }
